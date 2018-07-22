b0c7452c4d9a0058b8408d9a583a867269f6c976
lnnlmario <lnnlmario@163.com>
2018-07-22 21:22:55 +0800

    配置项常量类增加注释

--- smartframework/src/main/java/com/wingwang/framework/ConfigConstant.java
+++ smartframework/src/main/java/com/wingwang/framework/ConfigConstant.java
@@ -11,7 +11,10 @@ public interface ConfigConstant {
     String JDBC_USERNAME = "smart.framework.jdbc.username";
     String JDBC_PASSWORD = "smart.framework.jdbc.password";
 
+    // 项目基础包名
     String APP_BASE_PACKAGE = "smart.framework.app.base_package";
+    // JSP基础路径
     String APP_JSP_PATH = "smart.framework.app.jsp_path";
+    // 静态资源的基础路径，如JS，CSS，图片等
     String APP_ASSERT_PATH = "smart.framework.app.assert_path";
 }

2bde8597d233c41f9032ff7d2181a375761c1c30
lnnlmario <lnnlmario@163.com>
2018-08-18 00:53:35 +0800

    增加切面注解

---
+++ smartframework/src/main/java/com/wingwang/framework/annotation/Aspect.java
@@ -0,0 +1,10 @@
+package com.wingwang.framework.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspect {
+
+    Class<? extends Annotation> value();
+}

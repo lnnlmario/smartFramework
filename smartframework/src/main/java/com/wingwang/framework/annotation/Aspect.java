66814ccb445bafa302dc1ace4b992dee2b114348
lnnlmario <lnnlmario@163.com>
2018-08-18 01:22:42 +0800

    切面注解增加注释

--- smartframework/src/main/java/com/wingwang/framework/annotation/Aspect.java
+++ smartframework/src/main/java/com/wingwang/framework/annotation/Aspect.java
@@ -2,6 +2,9 @@ package com.wingwang.framework.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 切面注解
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Aspect {

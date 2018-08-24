ae9496bd073e61205078e02bd88edbd14bd89b00
lnnlmario <lnnlmario@163.com>
2018-08-25 01:13:40 +0800

    在框架中添加事务代理机制

--- smartframework/src/main/java/com/wingwang/framework/helper/AopHelper.java
+++ smartframework/src/main/java/com/wingwang/framework/helper/AopHelper.java
@@ -1,9 +1,11 @@
 package com.wingwang.framework.helper;
 
 import com.wingwang.framework.annotation.Aspect;
+import com.wingwang.framework.annotation.Service;
 import com.wingwang.framework.proxy.AspectProxy;
 import com.wingwang.framework.proxy.Proxy;
 import com.wingwang.framework.proxy.ProxyManager;
+import com.wingwang.framework.proxy.TransactionProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,6 +68,12 @@ public final class AopHelper {
      */
     private static Map<Class<?>, Set<Class<?>>> createProxyMap() throws Exception{
         Map<Class<?>, Set<Class<?>>> proxyMap = new HashMap<Class<?>, Set<Class<?>>>();
+        addAspectProxy(proxyMap);
+        addTransactionProxy(proxyMap);
+        return proxyMap;
+    }
+
+    private static void addAspectProxy(Map<Class<?>, Set<Class<?>>> proxyMap) throws Exception {
         Set<Class<?>> proxyClassSet = ClassHelper.getClassSetBySuper(AspectProxy.class);
         for (Class<?> proxyClass : proxyClassSet) {
             if (proxyClass.isAnnotationPresent(Aspect.class)) {
@@ -74,7 +82,11 @@ public final class AopHelper {
                 proxyMap.put(proxyClass, targetClassSet);
             }
         }
-        return proxyMap;
+    }
+
+    private static void addTransactionProxy(Map<Class<?>, Set<Class<?>>> proxyMap) throws Exception {
+        Set<Class<?>> serviceClassSet = ClassHelper.getClassSetByAnnotation(Service.class);
+        proxyMap.put(TransactionProxy.class, serviceClassSet);
     }
 
     /**
